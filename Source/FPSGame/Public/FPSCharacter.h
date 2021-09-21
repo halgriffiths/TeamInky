ec4cf15a9ae4ea7c1e118f1a70cd6c58bc7f181a
Seyoung Lee <seyounglee@improbable.io>
2021-09-21 17:42:12 +0100

    [Fix] SpatialOS Settings

--- Source/FPSGame/Public/FPSCharacter.h
+++ Source/FPSGame/Public/FPSCharacter.h
@@ -22,17 +22,6 @@ class AFPSCharacter : public ACharacter
 
 protected:
 
-	/** Pawn mesh: 1st person view  */
-	/*
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Mesh")
-	USkeletalMeshComponent* Mesh1PComponent;
-
-	/** Gun mesh: 1st person view (seen only by self) */
-	/*
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Mesh")
-	USkeletalMeshComponent* GunMeshComponent;
-	*/
-	/** First person camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
 	UCameraComponent* CameraComponent;
 
